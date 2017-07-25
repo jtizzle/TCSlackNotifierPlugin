6d633615c138313a69cf294a616bffc0fce11dce
John Hinton <jh011889@gmail.com>
2017-07-25 14:54:30 -0400

    use an attachment

--- src/main/java/com/tapadoo/slacknotifier/SlackServerAdapter.java
+++ src/main/java/com/tapadoo/slacknotifier/SlackServerAdapter.java
@@ -32,39 +32,35 @@ public class SlackServerAdapter extends BuildServerAdapter {
     private final SBuildServer buildServer;
     private final SlackConfigProcessor slackConfig;
     private final ProjectSettingsManager projectSettingsManager;
-    private final ProjectManager projectManager ;
-    private Gson gson ;
+    private final ProjectManager projectManager;
+    private Gson gson;
 
-    public SlackServerAdapter(SBuildServer sBuildServer, ProjectManager projectManager, ProjectSettingsManager projectSettingsManager , SlackConfigProcessor configProcessor) {
+    public SlackServerAdapter(SBuildServer sBuildServer, ProjectManager projectManager, ProjectSettingsManager projectSettingsManager, SlackConfigProcessor configProcessor) {
 
-        this.projectManager = projectManager ;
-        this.projectSettingsManager = projectSettingsManager ;
-        this.buildServer = sBuildServer ;
-        this.slackConfig = configProcessor ;
+        this.projectManager = projectManager;
+        this.projectSettingsManager = projectSettingsManager;
+        this.buildServer = sBuildServer;
+        this.slackConfig = configProcessor;
 
     }
 
-    public void init()
-    {
+    public void init() {
         buildServer.addListener(this);
     }
 
-    private Gson getGson()
-    {
-        if( gson == null )
-        {
-            gson = new GsonBuilder().create() ;
+    private Gson getGson() {
+        if (gson == null) {
+            gson = new GsonBuilder().create();
         }
 
-        return gson ;
+        return gson;
     }
 
     @Override
     public void buildStarted(SRunningBuild build) {
         super.buildStarted(build);
 
-        if( !build.isPersonal() && slackConfig.postStarted() )
-        {
+        if (!build.isPersonal() && slackConfig.postStarted()) {
             postStartedBuild(build);
         }
     }
@@ -73,33 +69,27 @@ public class SlackServerAdapter extends BuildServerAdapter {
     public void buildFinished(SRunningBuild build) {
         super.buildFinished(build);
 
-        if( !build.isPersonal() && build.getBuildStatus().isSuccessful() && slackConfig.postSuccessful() )
-        {
+        if (!build.isPersonal() && build.getBuildStatus().isSuccessful() && slackConfig.postSuccessful()) {
             processSuccessfulBuild(build);
-        }
-        else if ( !build.isPersonal() && build.getBuildStatus().isFailed() && slackConfig.postFailed() )
-        {
+        } else if (!build.isPersonal() && build.getBuildStatus().isFailed() && slackConfig.postFailed()) {
             postFailureBuild(build);
-        }
-        else
-        {
+        } else {
             //TODO - modify in future if we care about other states
         }
     }
 
-    private void postStartedBuild(SRunningBuild build )
-    {
+    private void postStartedBuild(SRunningBuild build) {
         //Could put other into here. Agents maybe?
-        String message = "STARTED " + build.getFullName() + " (" + build.getBranch().getName() + ")" + " #" + build.getBuildNumber()+"\n";
+        String message = "STARTED " + build.getFullName() + " (" + build.getBranch().getName() + ")" + " #" + build.getBuildNumber() + "\n";
         message += "Agent: " + build.getAgent().getHostName() + " : " + build.getAgent().getHostAddress() + "\n";
         if (build.getBuildComment() != null) {
             message += build.getBuildComment().getUser() + " " + build.getBuildComment().getComment();
         }
         //String message = String.format("'%s' (%s) build started on Agent: %s." , build.getFullName(), build.getBranch(), build.getAgent().getHostName());
-        postToSlack(build, message, true);
+        postToSlack(build, message, "#1dacd6");
     }
-    private void postFailureBuild(SRunningBuild build )
-    {
+
+    private void postFailureBuild(SRunningBuild build) {
         String message = "";
 
         PeriodFormatter durationFormatter = new PeriodFormatterBuilder()
@@ -115,9 +105,9 @@ public class SlackServerAdapter extends BuildServerAdapter {
                 .appendSuffix(" second", " seconds")
                 .toFormatter();
 
-        Duration buildDuration = new Duration(1000*build.getDuration());
+        Duration buildDuration = new Duration(1000 * build.getDuration());
 
-        message += "FAILURE " + build.getFullName() + " (" + build.getBranch().getName() + ")" + " #" + build.getBuildNumber()+"\n";
+        message += "FAILURE " + build.getFullName() + " (" + build.getBranch().getName() + ")" + " #" + build.getBuildNumber() + "\n";
         message += "Agent: " + build.getAgent().getHostName() + " : " + build.getAgent().getHostAddress() + "\n";
         message += "Build failed in " + durationFormatter.print(buildDuration.toPeriod());
         if (build.getBuildComment() != null) {
@@ -125,7 +115,7 @@ public class SlackServerAdapter extends BuildServerAdapter {
         }
         //message = String.format("'%s' (%s) build failed on Agent: %s! (%s)" , build.getFullName(), build.getBranch().getDisplayName(), build.getAgent().getHostName() ,durationFormatter.print(buildDuration.toPeriod()));
 
-        postToSlack(build, message, false);
+        postToSlack(build, message, "danger");
     }
 
     private void processSuccessfulBuild(SRunningBuild build) {
@@ -145,9 +135,9 @@ public class SlackServerAdapter extends BuildServerAdapter {
                 .appendSuffix(" second", " seconds")
                 .toFormatter();
 
-        Duration buildDuration = new Duration(1000*build.getDuration());
+        Duration buildDuration = new Duration(1000 * build.getDuration());
 
-        message += "SUCCESS " + build.getFullName() + " (" + build.getBranch().getName() + ")" +  " #" + build.getBuildNumber()+"\n";
+        message += "SUCCESS " + build.getFullName() + " (" + build.getBranch().getName() + ")" + " #" + build.getBuildNumber() + "\n";
         message += "Agent: " + build.getAgent().getHostName() + " : " + build.getAgent().getHostAddress() + "\n";
         message += "Build succeeded in " + durationFormatter.print(buildDuration.toPeriod());
         if (build.getBuildComment() != null) {
@@ -155,99 +145,88 @@ public class SlackServerAdapter extends BuildServerAdapter {
         }
         //message = String.format("'%s' (%s) built successfully in %s on Agent: %s." , build.getFullName(), build.getBranch().getDisplayName(), durationFormatter.print(buildDuration.toPeriod()), build.getAgent().getHostName());
 
-        postToSlack(build, message, true);
+        postToSlack(build, message, "good");
     }
 
     /**
      * Post a payload to slack with a message and good/bad color. Committer summary is automatically added as an attachment
-     * @param build the build the message is relating to
-     * @param message main message to include, 'Build X completed...' etc
-     * @param goodColor true for 'good' builds, false for danger.
+     *
+     * @param build     the build the message is relating to
+     * @param message   main message to include, 'Build X completed...' etc
      */
-    private void postToSlack(SRunningBuild build, String message, boolean goodColor) {
-        try{
+    private void postToSlack(SRunningBuild build, String message, String color) {
+        try {
 
             URL url = new URL(slackConfig.getPostUrl());
 
-            SlackProjectSettings projectSettings = (SlackProjectSettings) projectSettingsManager.getSettings(build.getProjectId(),"slackSettings");
+            SlackProjectSettings projectSettings = (SlackProjectSettings) projectSettingsManager.getSettings(build.getProjectId(), "slackSettings");
 
-            if( ! projectSettings.isEnabled() )
-            {
-                return ;
+            if (!projectSettings.isEnabled()) {
+                return;
             }
 
             String iconUrl = projectSettings.getLogoUrl();
 
-            if(iconUrl == null || iconUrl.length() < 1 )
-            {
-                iconUrl = slackConfig.getLogoUrl() ;
+            if (iconUrl == null || iconUrl.length() < 1) {
+                iconUrl = slackConfig.getLogoUrl();
             }
 
             String configuredChannel = build.getParametersProvider().get("SLACK_CHANNEL");
             String channel = this.slackConfig.getDefaultChannel();
 
-            if( configuredChannel != null && configuredChannel.length() > 0 )
-            {
-                channel = configuredChannel ;
-            }
-            else if( projectSettings != null && projectSettings.getChannel() != null && projectSettings.getChannel().length() > 0 )
-            {
-                channel = projectSettings.getChannel() ;
+            if (configuredChannel != null && configuredChannel.length() > 0) {
+                channel = configuredChannel;
+            } else if (projectSettings != null && projectSettings.getChannel() != null && projectSettings.getChannel().length() > 0) {
+                channel = projectSettings.getChannel();
             }
 
             UserSet<SUser> committers = build.getCommitters(SelectPrevBuildPolicy.SINCE_LAST_BUILD);
             StringBuilder committersString = new StringBuilder();
 
-            for( SUser committer : committers.getUsers() )
-            {
-                if( committer != null)
-                {
-                    String committerName = committer.getName() ;
-                    if( committerName == null || committerName.equals("") )
-                    {
-                        committerName = committer.getUsername() ;
+            for (SUser committer : committers.getUsers()) {
+                if (committer != null) {
+                    String committerName = committer.getName();
+                    if (committerName == null || committerName.equals("")) {
+                        committerName = committer.getUsername();
                     }
 
-                    if( committerName != null && !committerName.equals(""))
-                    {
+                    if (committerName != null && !committerName.equals("")) {
                         committersString.append(committerName);
                         committersString.append(",");
                     }
                 }
             }
 
-            if( committersString.length() > 0 )
-            {
-                committersString.deleteCharAt(committersString.length()-1); //remove the last ,
+            if (committersString.length() > 0) {
+                committersString.deleteCharAt(committersString.length() - 1); //remove the last ,
             }
 
             String commitMsg = committersString.toString();
 
 
             JsonObject payloadObj = new JsonObject();
-            payloadObj.addProperty("channel" , channel);
-            payloadObj.addProperty("username" , "TeamCity");
-            payloadObj.addProperty("text", message);
-            payloadObj.addProperty("icon_url",iconUrl);
+            payloadObj.addProperty("channel", channel);
+            payloadObj.addProperty("username", "TeamCity");
+//            payloadObj.addProperty("text", "Build status...");
+            payloadObj.addProperty("icon_url", iconUrl);
 
             JsonArray attachmentsObj = new JsonArray();
 
-            if( commitMsg.length() > 0 )
-            {
+            if (commitMsg.length() > 0) {
                 JsonObject attachment = new JsonObject();
 
-                attachment.addProperty("fallback", "Changes by"+ commitMsg);
-                attachment.addProperty("color",( goodColor ? "good" : "danger"));
+                attachment.addProperty("fallback", "Changes by" + commitMsg);
+                attachment.addProperty("color", color);
 
                 JsonArray fields = new JsonArray();
-                JsonObject field = new JsonObject() ;
+                JsonObject field = new JsonObject();
 
-                field.addProperty("title","Changes By");
-                field.addProperty("value",commitMsg);
+                field.addProperty("title", "Changes By");
+                field.addProperty("value", commitMsg);
                 field.addProperty("short", true);
 
                 fields.add(field);
-                attachment.add("fields",fields);
+                attachment.add("fields", fields);
 
                 attachmentsObj.add(attachment);
 
@@ -255,8 +234,7 @@ public class SlackServerAdapter extends BuildServerAdapter {
 
             //Do we have any issues?
 
-            if( build.isHasRelatedIssues() )
-            {
+            if (build.isHasRelatedIssues()) {
                 //We do!
                 Collection<Issue> issues = build.getRelatedIssues();
                 JsonObject issuesAttachment = new JsonObject();
@@ -264,8 +242,7 @@ public class SlackServerAdapter extends BuildServerAdapter {
                 StringBuilder issueIds = new StringBuilder();
                 StringBuilder clickableIssueIds = new StringBuilder();
 
-                for( Issue issue : issues )
-                {
+                for (Issue issue : issues) {
                     issueIds.append(',');
                     issueIds.append(issue.getId());
 
@@ -278,25 +255,23 @@ public class SlackServerAdapter extends BuildServerAdapter {
                     clickableIssueIds.append('>');
                 }
 
-                if( issueIds.length() > 0 )
-                {
+                if (issueIds.length() > 0) {
                     issueIds.deleteCharAt(0); //delete first ','
                 }
 
-                if( clickableIssueIds.length() > 0 )
-                {
+                if (clickableIssueIds.length() > 0) {
                     clickableIssueIds.deleteCharAt(0); //delete first ','
                 }
 
-                issuesAttachment.addProperty("fallback" , "Issues " + issueIds.toString());
+                issuesAttachment.addProperty("fallback", "Issues " + issueIds.toString());
                 //Not sure what color, if any to use for this. For now, leave it the same as the committers one
-                issuesAttachment.addProperty("color",( goodColor ? "good" : "danger"));
+                issuesAttachment.addProperty("color", color);
 
                 JsonArray fields = new JsonArray();
-                JsonObject field = new JsonObject() ;
+                JsonObject field = new JsonObject();
 
-                field.addProperty("title","Related Issues");
-                field.addProperty("value",clickableIssueIds.toString());
+                field.addProperty("title", "Related Issues");
+                field.addProperty("value", clickableIssueIds.toString());
                 field.addProperty("short", true);
 
                 fields.add(field);
@@ -305,7 +280,27 @@ public class SlackServerAdapter extends BuildServerAdapter {
                 attachmentsObj.add(issuesAttachment);
             }
 
-            if( attachmentsObj.size() > 0 ) {
+            //
+            JsonObject attachment = new JsonObject();
+
+            attachment.addProperty("fallback", "build");
+            attachment.addProperty("color", color);
+
+            JsonArray fields = new JsonArray();
+            JsonObject field = new JsonObject();
+
+            field.addProperty("title", build.getFullName());
+            field.addProperty("value", message);
+            field.addProperty("short", true);
+
+            fields.add(field);
+            attachment.add("fields", fields);
+
+            attachmentsObj.add(attachment);
+
+            //
+
+            if (attachmentsObj.size() > 0) {
                 payloadObj.add("attachments", attachmentsObj);
             }
 
@@ -315,20 +310,18 @@ public class SlackServerAdapter extends BuildServerAdapter {
             BufferedOutputStream bos = new BufferedOutputStream(conn.getOutputStream());
 
             String payloadJson = getGson().toJson(payloadObj);
-            String bodyContents = "payload=" + payloadJson ;
+            String bodyContents = "payload=" + payloadJson;
             bos.write(bodyContents.getBytes("utf8"));
             bos.flush();
             bos.close();
 
-            int serverResponseCode = conn.getResponseCode() ;
+            int serverResponseCode = conn.getResponseCode();
 
             conn.disconnect();
-            conn = null ;
-            url = null ;
+            conn = null;
+            url = null;
 
-        }
-        catch ( MalformedURLException ex )
-        {
+        } catch (MalformedURLException ex) {
 
         } catch (IOException e) {
             e.printStackTrace();
